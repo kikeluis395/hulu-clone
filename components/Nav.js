76f5c74a84d307df11e491e7187a43e8f3c76116
Luis Alvarado <kikeluis395@gmail.com>
2021-07-12 00:10:13 -0500

    mostrar peliculas y grid terminado

--- components/Nav.js
+++ components/Nav.js
@@ -3,7 +3,6 @@ import requests from "../utils/requests";
 
 function Nav() {
   const router = useRouter();
-  console.log(router);
   return (
     <nav className="relative">
       <div className="flex px-10 sm:px-20 text-2xl whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-scroll scrollbar-hide">
